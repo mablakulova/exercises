7a4a6428fabc371ddead27cd4ed71a75f2ee0a7b
mablakulova <abmakhliyo@gmail.com>
2024-05-11 08:18:41 +0500

    add turnstile by id report

--- sql-report/ssp_function.sql
+++ sql-report/ssp_function.sql
@@ -250,3 +250,218 @@ BEGIN
 
 END;
 $BODY$;
+
+--DROP FUNCTION IF EXISTS hrm.get_employee_turnstile_report_by_id(timestamp without time zone, integer, integer, integer);
+
+CREATE OR REPLACE FUNCTION hrm.get_employee_turnstile_report_by_id_test1(
+	in_start_date timestamp without time zone,
+	in_end_date timestamp without time zone,
+	in_org_id integer,
+	in_language_id integer,
+	in_employee_id integer,
+	in_start_work_time interval,
+	in_end_work_time interval
+)
+RETURNS TABLE (
+    employee_id integer, 
+    event_on date, 
+    week_day text, 
+    enter_at text, 
+    exit_at text, 
+    period_minute integer, 
+    enter_at_schedule text, 
+    exit_at_schedule text, 
+    period_minute_schedule integer
+) 
+LANGUAGE 'plpgsql'
+COST 100
+VOLATILE PARALLEL UNSAFE
+ROWS 1000
+AS $BODY$
+
+BEGIN 
+   RETURN QUERY
+   WITH cte
+   AS (
+	SELECT 
+        DISTINCT employeemanage.employee_id,
+		(employeelog.event_At - INTERVAL '7 hours')::DATE AS eventon,
+		employeelog.employee_turnstile_log_type_id AS action_type,
+		TRIM(BOTH ' ' FROM TO_CHAR((employeelog.event_At - INTERVAL '7 hours')::DATE, 'Day')) AS week_day,
+		DATE_TRUNC('minute', employeelog.event_at) AS event_at,
+		COALESCE(in_start_work_time, scheduledayhour.begin_at::interval, '09:00'::interval) AS begin_schedule_time,
+		COALESCE(in_end_work_time, scheduledayhour.end_at::interval, '18:00'::interval) AS end_schedule_time,
+	    ROW_NUMBER() OVER (PARTITION BY employeelog.employee_id, (employeelog.event_At - INTERVAL '7 hours')::DATE ORDER BY employeelog.event_at) AS rn
+    FROM 
+        hrm.sys_employee_turnstile_log employeelog 
+    LEFT JOIN 
+        hrm.sys_employee_manage employeemanage ON employeelog.employee_id = employeemanage.employee_id 
+    LEFT JOIN 
+        hrm.hl_employee employee ON employeemanage.employee_id = employee.id 
+    LEFT JOIN 
+        public.hl_person person ON employee.person_id = person.id
+    LEFT JOIN 
+        hrm.info_work_schedule schedule ON schedule.id = employeemanage.work_schedule_id 
+    LEFT JOIN 
+        hrm.info_work_schedule_day_hour scheduledayhour ON scheduledayhour.owner_id = schedule.id AND
+        scheduledayhour.day_number = EXTRACT(ISODOW FROM (employeelog.event_at - INTERVAL '7 hours')::DATE::TIMESTAMP)
+    WHERE
+	    employeelog.employee_id = in_employee_id
+        employeemanage.start_on < in_end_date::DATE AND
+	    (employeemanage.end_on IS NULL OR employeemanage.end_on > in_end_date::DATE) AND
+        employeemanage.is_deleted = false AND
+        employee.organization_id = in_org_id AND
+        (employeelog.event_at - INTERVAL '7 hours')::DATE >= in_start_date::DATE AND 
+        (employeelog.event_at - INTERVAL '7 hours')::DATE <= in_end_date::DATE	
+	),
+    cte2
+    AS (
+	  SELECT 
+        a.employee_id,
+		a.starton,
+		a.eventon,
+		a.week_day,
+		CASE 
+			WHEN CASE 
+					WHEN a.begin_schedule_time >= TO_CHAR(CASE 
+								WHEN entertime.event_at IS NULL
+								    THEN LAG(exittime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY exittime.rn)
+								ELSE entertime.event_at
+								END, 'HH24:MI')::interval
+						THEN a.begin_schedule_time
+					ELSE TO_CHAR(CASE 
+								WHEN entertime.event_at IS NULL
+								    THEN LAG(exittime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY exittime.rn)
+								ELSE entertime.event_at
+								END, 'HH24:MI')::interval
+					END >= a.end_schedule_time
+				THEN a.end_schedule_time
+			ELSE CASE 
+					WHEN a.begin_schedule_time >= TO_CHAR(CASE 
+								WHEN entertime.event_at IS NULL
+									THEN LAG(exittime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY exittime.rn)
+								ELSE entertime.event_at
+								END, 'HH24:MI')::interval
+						THEN a.begin_schedule_time
+					ELSE TO_CHAR(CASE 
+								WHEN entertime.event_at IS NULL
+								    THEN LAG(exittime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY exittime.rn)
+								ELSE entertime.event_at
+								END, 'HH24:MI')::interval
+					END
+			END AS enterschedule,
+		CASE 
+			WHEN a.end_schedule_time <= TO_CHAR(CASE 
+						WHEN exittime.event_at IS NULL
+						    THEN LEAD(entertime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY entertime.rn)
+						ELSE exittime.event_at
+						END, 'HH24:MI')::interval
+				OR TO_CHAR(CASE 
+						WHEN exittime.event_at IS NULL
+							THEN LEAD(entertime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY entertime.rn)
+						ELSE exittime.event_at
+						END, 'HH24:MI')::interval <= '07:00'::interval
+				THEN a.end_schedule_time
+			ELSE TO_CHAR(CASE 
+						WHEN exittime.event_at IS NULL
+							THEN LEAD(entertime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY entertime.rn)
+						ELSE exittime.event_at
+						END, 'HH24:MI')::interval
+			END AS exitschedule,
+		CASE 
+			WHEN entertime.event_at IS NULL
+				THEN LAG(exittime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY exittime.rn)
+			ELSE entertime.event_at
+			END AS entertime,
+		CASE 
+			WHEN exittime.event_at IS NULL
+               THEN LEAD(entertime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY entertime.rn)
+			ELSE exittime.event_at
+			END AS exittime
+		FROM cte a 
+		LEFT JOIN (
+			SELECT cte.employee_id,
+			       event_at,
+			       eventon,
+			       rn 
+		    FROM cte 
+		    WHERE action_type = 1
+		    ) AS entertime
+		    ON a.employee_id = entertime.employee_id AND a.rn = entertime.rn AND a.eventon = entertime.eventon
+	    LEFT JOIN (
+			SELECT cte.employee_id,
+			       event_at,
+			       eventon,
+			       rn 
+			FROM cte 
+			WHERE action_type = 2
+		    ) AS exittime
+		    ON a.employee_id = exittime.employee_id AND a.rn = exittime.rn - 1 AND a.eventon = exittime.eventon
+		ORDER BY CASE 
+			WHEN entertime.event_at IS NULL
+		        THEN LAG(exittime.event_at) OVER (PARTITION BY a.employee_id, a.eventon ORDER BY exittime.rn)
+			ELSE entertime.event_at
+			END
+	),
+    cte3
+      AS (
+        SELECT 
+	        cte2.employee_id,
+		    cte2.eventon,
+            MIN(entertime) AS first_entertime,
+            MAX(exittime) AS last_exittime,
+            MIN(enterschedule) AS first_enterschedule,
+            MAX(exitschedule) AS last_exitschedule
+        FROM 
+            cte2
+        WHERE 
+            (entertime IS NOT NULL AND exittime IS NOT NULL) AND 
+	        (entertime <> exittime)
+	    GROUP BY 
+            cte2.employee_id, cte2.eventon
+       ),
+    cte4
+      AS (
+        SELECT 
+            cte2.*,
+	        cte3.first_entertime,
+	        cte3.last_exittime,
+	        cte3.first_enterschedule, 
+	        cte3.last_exitschedule,
+            (DATE_PART('day', cte2.exittime - cte2.entertime) * 24 + DATE_PART('hour', cte2.exittime - cte2.entertime) * 60 + 
+             DATE_PART('minute', cte2.exittime - cte2.entertime)) AS periodMinute,
+            (DATE_PART('day', cte2.exitschedule - cte2.enterschedule) * 24 + DATE_PART('hour', cte2.exitschedule - cte2.enterschedule) * 60 + 
+             DATE_PART('minute', cte2.exitschedule - cte2.enterschedule)) AS periodMinuteSchedule
+        FROM
+           cte2
+        JOIN 
+           cte3 ON cte2.employee_id = cte3.employee_id AND cte2.eventon = cte3.eventon
+        WHERE 
+           (cte2.entertime IS NOT NULL AND cte2.exittime IS NOT NULL) AND 
+           (cte2.entertime <> cte2.exittime)
+      )
+    SELECT
+        cte4.employee_id,
+        TO_CHAR(cte4.eventon, 'DD.MM.YYYY') AS event_on,
+        cte4.week_day,
+        TO_CHAR(cte4.first_entertime, 'HH24:MI:SS') AS enter_at,
+        TO_CHAR(cte4.last_exittime, 'HH24:MI:SS') AS exit_at,
+		CAST(SUM(cte4.periodMinute) AS INTEGER) AS period_minute,
+        TO_CHAR(cte4.first_enterschedule, 'HH24:MI:SS') AS enter_at_schedule,
+        TO_CHAR(cte4.last_exitschedule, 'HH24:MI:SS') AS exit_at_schedule,
+        CAST(SUM(cte4.periodMinuteSchedule) AS INTEGER) AS period_minute_schedule
+    FROM
+        cte4
+    GROUP BY
+        cte4.employee_id,
+        cte4.eventon,
+	    cte4.first_entertime,
+	    cte4.last_exittime,
+	    cte4.first_enterschedule,
+	    cte4.last_exitschedule,
+        cte4.week_day
+    ORDER BY
+        cte4.eventon;
+
+END;
+$BODY$;
